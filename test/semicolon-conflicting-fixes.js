beeaed035229f231488dbda09bd664a4dabf43d2
Cameron Little <cameron@camlittle.com>
2020-11-03 20:26:05 +0100

    Document source of test files

--- test/semicolon-conflicting-fixes.js
+++ test/semicolon-conflicting-fixes.js
@@ -1,3 +1,5 @@
+// https://github.com/eslint/eslint/tree/4d35a81c9ddae2e77409bb6d82adeb62e1e1c33c/tests/fixtures/autofix
+
 /* eslint semi: ["error", "never"] */
 /* eslint no-extra-semi: "error" */
 var foo = function(){};
